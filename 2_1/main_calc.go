4575363286fce29499072077d1e685a218773b8b
Andreygolpy <182397185+Andrey-Zobnin@users.noreply.github.com>
2025-05-21 18:11:51 +0300

    chage plase where output number and str

--- 2_1/main_calc.go
+++ 2_1/main_calc.go
@@ -3,6 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(2, "#")
+	fmt.Println("#", 2)
 	// Запомните: в Println аргументы всегда выводятся через пробел, Print добавляется пробел только между числами
 }
