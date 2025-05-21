9061a4a1a0dc09fdd2280af6b48cebcfe178f2fa
Andreygolpy <182397185+Andrey-Zobnin@users.noreply.github.com>
2025-05-21 18:16:48 +0300

    output calc like that

--- 2_1/main_calc.go
+++ 2_1/main_calc.go
@@ -3,6 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(2, "#", 3, "#", 4, "#", 5)
+	fmt.Println("5 + 7 =", 12)
 	// Запомните: в Println аргументы всегда выводятся через пробел, Print добавляется пробел только между числами
 }
