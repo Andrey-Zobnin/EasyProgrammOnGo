a8286ccaf94e75b0fe07cf8802f3ec218331747a
Andrey-Zobnin <andy.zobnin@gmail.com>
2025-05-27 11:55:16 +0300

    solution wit correct scan input, bufio

--- 2_3/input _data_task2.go
+++ 2_3/input _data_task2.go
@@ -1,17 +1,20 @@
 package main
 
-import (
-	"bufio"
+import ( 
 	"fmt"
 	"os"
+	"bufio"
 	"strings"
 )
+var lines [3]string
 
 func main() {
-	read := bufio.NewReaderSize(os.Stdin)
-	metaData, _ := read.ReadString('\n')
-	metaData = strings.TrimSpace(metaData)
-	// output
-	fmt.Println(metaData)
-
+    scan := bufio.NewScanner(os.Stdin)
+	for i := 0; i < 3 i++ {
+		scan.Scan()
+		lines[i] = scan.Text()
+	}
+	for _, line := range lines {
+		fmat.Println(line)
+	}
 }
