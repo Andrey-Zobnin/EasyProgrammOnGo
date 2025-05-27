140ff6364c0c1b77294a5d836c0829c159caf419
Andrey-Zobnin <andy.zobnin@gmail.com>
2025-05-27 11:15:14 +0300

    global -value

--- 2_3/main.go
+++ 2_3/main.go
@@ -2,11 +2,12 @@ package main
 
 import ("fmt") 
 
+var a string
 
 func main() {
-	var a string
+	// a := ""
 	// указатель на переменную принимает команда fmt.Scan(&a)
 	fmt.Scan(&a)
 	fmt.Print("Привет, ", a)
-
+	
 }
