ee9a096dff86fd3abf9bf6bd751e642cb5674f70
Andrey-Zobnin <andy.zobnin@gmail.com>
2025-05-27 11:56:03 +0300

    refactor some val and replase line with count lines

--- 2_3/input _data_task2.go
+++ 2_3/input _data_task2.go
@@ -1,20 +1,19 @@
 package main
 
-import ( 
+import (
+	"bufio"
 	"fmt"
 	"os"
-	"bufio"
-	"strings"
 )
-var lines [3]string
 
 func main() {
-    scan := bufio.NewScanner(os.Stdin)
-	for i := 0; i < 3 i++ {
+	scan := bufio.NewScanner(os.Stdin)
+	var lines [3]string
+	for i := 0; i < 3; i++ {
 		scan.Scan()
 		lines[i] = scan.Text()
 	}
 	for _, line := range lines {
-		fmat.Println(line)
+		fmt.Println(line)
 	}
 }
