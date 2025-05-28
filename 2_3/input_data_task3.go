e41ec86c99792d21a8ea68eef4f7bdaba218c3bd
Andrey-Zobnin <andy.zobnin@gmail.com>
2025-05-28 20:55:56 +0300

    slice, rename value, is worked

--- 2_3/input_data_task3.go
+++ 2_3/input_data_task3.go
@@ -8,11 +8,11 @@ import (
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
-	meta := make([]strings, 0, 3)
+	meta := make([]string, 0, 3)
 
 	for i := 0; i < 3; i++ {
 		scanner.Scan()
-		meta = append(lines, scanner.Text())
+		meta = append(meta, scanner.Text())
 	}
 
 	for i := len(meta) - 1; i >= 0; i-- {
