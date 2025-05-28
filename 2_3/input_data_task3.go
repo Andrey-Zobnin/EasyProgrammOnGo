e7464034766bbb8e105fb2d3b3e57f8c143e9cc5
Andrey-Zobnin <andy.zobnin@gmail.com>
2025-05-28 20:50:55 +0300

    add 3 scan and output in random order comment line code

--- 2_3/input_data_task3.go
+++ 2_3/input_data_task3.go
@@ -9,7 +9,19 @@ import (
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	//for i := 0; i > 0; i++ {}
-	_ = scanner.Scan()
-	meta := scanner.Text()
-	fmt.Println(meta)
+	// _ = scanner.Scan()
+
+	scanner.Scan()
+	meta1 := scanner.Text()
+
+	scanner.Scan()
+	meta2 := scanner.Text()
+
+	scanner.Scan()
+	meta3 := scanner.Text()
+	// output in random order
+	fmt.Println(meta1)
+	fmt.Println(meta3)
+	fmt.Println(meta2)
+
 }
