5dedcc47b33e874e72d952ea9bead73c076edc0e
Andrey-Zobnin <andy.zobnin@gmail.com>
2025-05-29 13:52:07 +0300

    init prototype for task num5

---
+++ 2_4/task5.go
@@ -0,0 +1,18 @@
+pacakage main 
+
+import (
+	"fmt"
+	"os"
+)
+
+
+func main() {
+	startValueInLoop := 0
+	sizeOfArray := 4
+	var inputMetaDataString [sizeOfArray]string 
+	fmt.Scan(&inputMetaDataString)
+
+	for i := startValueInLoop; i < sizeOfArray; i++ {
+		
+	}
+}
